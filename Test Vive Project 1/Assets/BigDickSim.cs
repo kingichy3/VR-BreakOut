0c8d0e1c234db168e6acc2c603620eb84b436478
kingichy3 <44006450+kingichy3@users.noreply.github.com>
2019-02-21 09:49:21 -0600

    put project in repository
    Need to change the name of some of these files

---
+++ Test Vive Project 1/Assets/BigDickSim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigDickSim : MonoBehaviour {
+
+    public GameObject head;
+    public float mult = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(head.transform.position.x + 0.214f, head.transform.position.y * mult, head.transform.position.z + -0.387f);
+        transform.rotation = Quaternion.Euler(0, 180 + head.transform.localRotation.eulerAngles.y, 0);
+	}
+}
